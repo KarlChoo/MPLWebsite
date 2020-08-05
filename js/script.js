fa3ea7e32fe85f4e756e9df9810f1c28706482cb
Karl Choo Jia Long <karlchoo37@gmail.com>
2020-08-06 04:01:12 +0800

    naming and links fixed
    -some file names mismatched
    -fix links and template cn

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ for (let index = 0; index < 100; index++) {
 */
 
 
-function changeLanguage(){
-    let currentURL =  window.location.href;
+function changeLanguage() {
+    let currentURL = window.location.href;
     let newURL;
 
     currentURL.includes("/en/") ?
-        newURL = currentURL.replace("/en/","/cn/").replace("-en","-cn"):
-        newURL = currentURL.replace("/cn/","/en/").replace("-cn","-en");
+        newURL = currentURL.replace("/en/", "/cn/").replace("-en", "-cn") :
+        newURL = currentURL.replace("/cn/", "/en/").replace("-cn", "-en");
     window.location.href = newURL;
 }
 
-function dropdownLink(link){
-    let shortlink = link.slice(link.indexOf("/cn/")).replace("/cn/","");
+function dropdownLink(link) {
+    let shortlink = link.slice(link.indexOf("/cn/")).replace("/cn/", "");
 
-    link.includes("/en/") ? 
-        shortlink = link.slice(link.indexOf("/en/")).replace("/en/",""):
-        shortlink = link.slice(link.indexOf("/cn/")).replace("/cn/","");
+    link.includes("/en/") ?
+        shortlink = link.slice(link.indexOf("/en/")).replace("/en/", "") :
+        shortlink = link.slice(link.indexOf("/cn/")).replace("/cn/", "");
     window.location.href = shortlink;
 }
